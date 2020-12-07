ba499eade730e90ace2d728e2343dcc52329fe0d
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2020-12-07 12:37:42 +0600

    just initialize gulp, gulp-sass, browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,3 @@
-// const gulp = require('gulp');
-// const sass = require('gulp-sass');
-// const browserSync = require('browser-sync').create();
-//
-// //Static server + watchng scss/html files
-// gulp.task ('serve', ['sass'], function () {
-//     browserSync.init({
-//         injectChanges: true,
-//         server: {
-//             baseDir: './dist'
-//         }
-//     });
-//
-//     gulp.watch('./src/sass/style.scss', ['sass']);
-//     gulp.watch('./dist/*.html').on('change', browserSync.reload);
-// });
-//
-// // Compile sass into CSS & auto-inject into browsers
-//
-// gulp.task('sass', function () {
-//     return gulp.src('./src/sass/style.scss')
-//         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-//         .pipe(gulp.dest('./dist/sass'))
-//         .pipe(browserSync.stream({match: '**/*.css'}));
-// })
-//
-// gulp.task('default', ['serve']);
+const gulp = require ('gulp');
+const sass = require ('gulp-sass');
+const browserSync = require ('browser-sync');
