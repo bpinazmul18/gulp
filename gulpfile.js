d064d20d9964ae0c44402d25f732b5293f90750b
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2020-12-07 13:55:22 +0600

    success first task

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,15 @@
-const gulp = require ('gulp');
-const sass = require ('gulp-sass');
-const browserSync = require ('browser-sync');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+//Get file and compressed than compile sass
+function gulpSass() {
+    return gulp
+        .src('./src/sass/style.scss')            //get file
+        .pipe(sass({                     //compile sass
+            outputStyle: 'compressed'          //output style compressed
+        }).on('error', sass.logError))        //sass log error
+        .pipe(gulp.dest('dist/css'))         //send file or output file
+}
+gulp.task(gulpSass)                        //task create
+
+gulp.task('start', gulp.series(gulpSass))//task execute
