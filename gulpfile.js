fe80040971786507b2e165a2e982f5bb35fd5ea9
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2020-12-07 15:52:20 +0600

    dist folder file transfer fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,15 @@
-// const gulp = require('gulp');
-// const browserSync = require('browser-sync').create();
-// const sass = require('gulp-sass');
-//
-// // Compile SASS
-// function style() {
-//     return gulp
-//         .src('./src/sass/style.scss')
-//         .pipe(sass({
-//             outputStyle: 'compressed'
-//         }).on('error', sass.logError))
-//         .pipe(gulp.dest("./src/css"))
-//         .pipe(browserSync.stream());
-// }
-// gulp.task(style);
-// // Watch SASS & Serve
-// function watch() {
-//     browserSync.init({
-//         server: {
-//             baseDir: "./src"
-//         }
-//     });
-//     gulp.watch(['./src/sass/*.scss'], style);
-//     gulp.watch("src/*.html").on('change', browserSync.reload);
-// }
-// gulp.task(watch);
-// gulp.task('start', gulp.series(style, watch));
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
 //FOR DIST FOLDER
+/*HTML*/
+function htmlForDIST() {
+    return gulp
+        .src('./src/*.html')
+        .pipe(gulp.dest('./dist/'))
+}
+gulp.task(htmlForDIST);
 /*SASS-COMPILER*/
 function sassCompilerForDIST() {
     return gulp
@@ -65,5 +45,4 @@ function streamForBrowserSYNC() {
     gulp.watch('./src/*.html').on('change', browserSync.reload);
 }
 gulp.task(streamForBrowserSYNC);
-
-gulp.task('start', gulp.series(sassCompilerForSRC, streamForBrowserSYNC))
+gulp.task('start', gulp.series(htmlForDIST, sassCompilerForDIST, sassCompilerForSRC, streamForBrowserSYNC))
